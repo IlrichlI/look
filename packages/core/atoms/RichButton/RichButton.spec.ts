9318ed04e471306f76734456ed925422f181af96
Mohammad khani <mohammadkhani722@gmail.com>
2023-05-22 13:13:48 +0430

    Simple test added

--- packages/core/atoms/RichButton/RichButton.spec.ts
+++ packages/core/atoms/RichButton/RichButton.spec.ts
@@ -1,11 +1,23 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import { RichButton } from './index'
 
+// you need to mock router
+vi.mock('i18next-vue', () => ({
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {})
+      }
+    }
+  }
+}))
+
 describe('RichButton', () => {
   it('renders properly', () => {
     const wrapper = mount(RichButton, { props: { type: 'secondary' } })
-    expect(wrapper.text()).toContain('')
+    expect(wrapper.findComponent(RichButton).isVisible()).toBe(true)
   })
 })
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), checker({ vueTsc: true /** or an object config */ })],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  },
   server: {
     proxy: {
       base: './',
